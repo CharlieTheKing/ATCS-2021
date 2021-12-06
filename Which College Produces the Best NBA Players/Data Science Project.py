bbf1cbc945a3d2bc6e061a4a07ee4d97020452cb
Charlie King <charlieking450@gmail.com>
2021-12-06 03:40:31 -0800

    Data Science Project

--- Which College Produces the Best NBA Players/Data Science Project.py
+++ Which College Produces the Best NBA Players/Data Science Project.py
@@ -8,21 +8,18 @@ df1 = pd.read_csv("CharlieKing_CSData - PlayersStats.csv")
 
 pd.set_option("display.max_columns", None)
 
+# get rid of return
 df2["PLAYER"] = df2["PLAYER"].replace('\n', ' ', regex=True)
 
-#merge by mutual player name
-df3 = pd.merge(df2, df1, how="inner", left_on="PLAYER", right_on="PLAYER")
+# merge by mutual player name
+data = pd.merge(df2, df1, how="inner", left_on="PLAYER", right_on="PLAYER")
 
-# print(df2["PLAYER"])
-# print(df1["PLAYER"])
-# print(df3.to_string())
-
-# only include players from the 6 schools with the most NBA players
-duke = df3[df3["COLLEGE"] == "Duke"]
-kentucky = df3[df3["COLLEGE"] == "Kentucky"]
-texas = df3[df3["COLLEGE"] == "Texas"]
-nova = df3[df3["COLLEGE"] == "Villanova"]
-ucla = df3[df3["COLLEGE"] == "UCLA"]
+# only include players from the 5 schools with the most NBA players
+duke = data[data["COLLEGE"] == "Duke"]
+kentucky = data[data["COLLEGE"] == "Kentucky"]
+texas = data[data["COLLEGE"] == "Texas"]
+nova = data[data["COLLEGE"] == "Villanova"]
+ucla = data[data["COLLEGE"] == "UCLA"]
 df = pd.merge(duke, kentucky, how='outer')
 df = pd.merge(df, texas, how='outer')
 df = pd.merge(df, nova, how='outer')
@@ -30,36 +27,35 @@ df = pd.merge(df, ucla, how='outer')
 
 # create new column with 1 as the value for each row so that when I group by school I can have the number of players
 player = 1
-df3['Players'] = player
+data['Players'] = player
 # print(df)
 
 # group players by college
-df5 = df.groupby("COLLEGE").sum()
+colleges = df.groupby("COLLEGE").sum()
 
 # pie chart of games played
-df5.plot.pie(y="GP")
+colleges.plot.pie(y="GP")
 plt.title("Top College's Sums of Player's Games Played in the NBA 2021 Season Relative to Each Other")
 
 # bar graph of games played
-# print(df5)
-ax = df5.plot.bar(y='GP', rot=0, color='lightcoral')
+ax = colleges.plot.bar(y='GP', rot=0, color='lightcoral')
 plt.title("Top College's Sums of Players' Games Played in the NBA 2021 Season")
 
 # pie chart of points
-df5.plot.pie(y="PTS")
+colleges.plot.pie(y="PTS")
 plt.title("Top College's Sums of Player's Points Per Game in the NBA 2021 Season Relative to Each Other")
 
 # bar graph of points
 # print(df5)
-ax = df5.plot.bar(y='PTS', rot=0, color='darkviolet')
+ax = colleges.plot.bar(y='PTS', rot=0, color='darkviolet')
 plt.title("Top College's Sums of Players' Points Per Game in the NBA 2021 Season")
 plt.show()
 
 # Isolate 8 colleges with the most players
-df15 = df3.groupby("COLLEGE").sum()
-df15 = df15['Players'].nlargest(12)
+topSchools = data.groupby("COLLEGE").sum()
+topSchools = topSchools['Players'].nlargest(12)
 
 # graph colleges with the most players
-ax = df15.plot.bar(y='Players', rot=0, color='mediumslateblue')
-plt.title("College's Number of NBA PLayers")
+ax = topSchools.plot.bar(y='Players', rot=0, color='mediumslateblue')
+plt.title("College's Number of NBA Players")
 plt.show()
