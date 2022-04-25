a6331764f69f76695b2253dc6d6394f3c70a0c5f
Charlie King <charlieking450@gmail.com>
2022-04-25 13:43:39 -0700

    Finished base game + graphics

--- Air Wars/main.py
+++ Air Wars/main.py
@@ -11,8 +11,9 @@ import random
 #constants
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 800
+RADIUS = 150
 SCREEN_TITLE = "Air Wars!"
-SCALING = 2.0
+SCALING = 0.2
 
 # Classes
 class FlyingSprite(arcade.Sprite):
@@ -53,18 +54,28 @@ class Welcome(arcade.Window):
         # Clear the screen and start drawing
         arcade.start_render()
 
+        # Draw a blue circle
+        arcade.draw_circle_filled(
+            SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, RADIUS, arcade.color.BLUE
+        )
+
+# Classes
 class AirWars(arcade.Window):
     """ The game """
 
-    def __init__(self, width, height, title):
+    def __init__(self):
         """Initialize the game
         """
-        super().__init__(width, height, title)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
         # Set up the empty sprite lists
         self.small_enemies_list = arcade.SpriteList()
         self.clouds_list = arcade.SpriteList()
         self.all_sprites = arcade.SpriteList()
+        self.paused = False
+        self.player = None
+        self.setup()
+        self.on_draw()
 
     def setup(self):
         """Get the game ready to play"""
@@ -84,6 +95,8 @@ class AirWars(arcade.Window):
         # Spawn new cloud every second
         arcade.schedule(self.add_cloud, 1.0)
 
+        self.on_update(float)
+
     def add_small_enemy(self, delta_time: float):
         """Adds a new enemy to the screen
 
@@ -92,17 +105,17 @@ class AirWars(arcade.Window):
         """
 
         # Create enemy sprite
-        enemy = FlyingSprite("images/bug.png", SCALING)
+        enemy = FlyingSprite("bug.png", SCALING/2)
 
         # Set its position to a random height and off screen right
         enemy.left = random.randint(self.width, self.width + 80)
         enemy.top = random.randint(10, self.height - 10)
 
         # Set its speed to a random speed heading left
-        enemy.velocity = (random.randint(-20, -5), 0)
+        enemy.velocity = (random.randint(-10, -5), 0)
 
         # Add it to the enemies list
-        self.enemies_list.append(enemy)
+        self.small_enemies_list.append(enemy)
         self.all_sprites.append(enemy)
 
     def add_cloud(self, delta_time: float):
@@ -113,7 +126,7 @@ class AirWars(arcade.Window):
         """
 
         # First, create the new cloud sprite
-        cloud = FlyingSprite("images/cloud.png", SCALING)
+        cloud = FlyingSprite("cloud.png", SCALING)
 
         # Set its position to a random height and off screen right
         cloud.left = random.randint(self.width, self.width + 80)
@@ -126,8 +139,96 @@ class AirWars(arcade.Window):
         self.clouds_list.append(cloud)
         self.all_sprites.append(cloud)
 
+    def on_key_press(self, symbol, modifiers):
+        """Handle user keyboard input
+        Q: Quit the game
+        P: Pause/Unpause the game
+        I/J/K/L: Move Up, Left, Down, Right
+        Arrows: Move Up, Left, Down, Right
+
+        Arguments:
+            symbol {int} -- Which key was pressed
+            modifiers {int} -- Which modifiers were pressed
+        """
+        if symbol == arcade.key.Q:
+            # Quit immediately
+            arcade.close_window()
+
+        if symbol == arcade.key.P:
+            self.paused = not self.paused
+
+        if symbol == arcade.key.I or symbol == arcade.key.UP:
+            self.player.change_y = 5
+
+        if symbol == arcade.key.K or symbol == arcade.key.DOWN:
+            self.player.change_y = -5
+
+        if symbol == arcade.key.J or symbol == arcade.key.LEFT:
+            self.player.change_x = -5
+
+        if symbol == arcade.key.L or symbol == arcade.key.RIGHT:
+            self.player.change_x = 5
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        """Undo movement vectors when movement keys are released
+
+        Arguments:
+            symbol {int} -- Which key was pressed
+            modifiers {int} -- Which modifiers were pressed
+        """
+        if (
+                symbol == arcade.key.I
+                or symbol == arcade.key.K
+                or symbol == arcade.key.UP
+                or symbol == arcade.key.DOWN
+        ):
+            self.player.change_y = 0
+
+        if (
+                symbol == arcade.key.J
+                or symbol == arcade.key.L
+                or symbol == arcade.key.LEFT
+                or symbol == arcade.key.RIGHT
+        ):
+            self.player.change_x = 0
+
+    def on_update(self, delta_time: float):
+        """Update the positions and statuses of all game objects
+        If paused, do nothing
+
+        Arguments:
+            delta_time {float} -- Time since the last update
+        """
+
+        # If paused, don't update anything
+        if self.paused:
+            return
+
+        # Did you hit anything? If so, end the game
+        if self.player.collides_with_list(self.small_enemies_list):
+            arcade.close_window()
+
+        # Update everything
+        self.all_sprites.update()
+
+        # Keep the player on screen
+        if self.player.top > self.height:
+            self.player.top = self.height
+        if self.player.right > self.width:
+            self.player.right = self.width
+        if self.player.bottom < 0:
+            self.player.bottom = 0
+        if self.player.left < 0:
+            self.player.left = 0
+
+    def on_draw(self):
+        """Draw all game objects
+        """
+        arcade.start_render()
+        self.all_sprites.draw()
+
 
 # Main code entry point
 if __name__ == "__main__":
-    app = Welcome()
+    app = AirWars()
     arcade.run()
